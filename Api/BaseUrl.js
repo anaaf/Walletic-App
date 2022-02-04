23028fe94f6ce1cc179ffdd91bcb1aad41e4e0f6
anaaf <anaaf5555@gmail.com>
2022-02-04 21:36:11 +0500

    relocated files to src

--- Api/BaseUrl.js
+++
@@ -1,3 +0,0 @@
-// export const baseUrl = 'http://192.168.5.113:3000/api';
-
-export const baseUrl = 'http://walletic-ap.herokuapp.com/api';
--- actions/AuthConstants.js
+++
@@ -1,5 +0,0 @@
-export const phoneVerfication = {
-  VERIFY_PHONE: 'VERIFY_PHONE',
-  VERIFY_CODE: 'VERIFY_CODE',
-  USER_DATA: 'USER_AUTH_INFO',
-};
