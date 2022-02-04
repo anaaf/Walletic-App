8c69c51f5b2051329ef67cab165926a73e62c51c
anaaf <anaaf5555@gmail.com>
2022-02-04 21:15:53 +0500

    added new reducers, ignore the index file changes

--- App.js
+++ App.js
@@ -1,19 +1,29 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import {Provider} from 'react-redux';
+import {Provider, ReactReduxContext} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
-import auth from './reducers/Auth';
+import auth from './src/redux/reducers/Auth';
+import QrScannerReducer from './src/redux/reducers/QRScannerReducer'
+import {QrDataSaveReducer, modalReducer} from './src/redux/reducers/QRScannerReducer'
 import {combineReducers} from 'redux';
 import Navigation from './Navigation/Navigation';
 import App1 from './App1';
+import store1 from './Playground/Redux/Store' // pg
+
 //combining the reducers
 const reducers = combineReducers({
-  auth: auth,
+  auth,
+  QrScannerReducer, 
+  QrDataSaveReducer,
+  modalReducer
 });
 const store = createStore(reducers, applyMiddleware(thunk));
 
 export default function App() {
+
+
+
   return (
     <Provider store={store}>
       <Navigation />
--- index.js
+++ index.js
@@ -3,8 +3,7 @@
  */
 import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
-import App from './src/Screens/QrGeneration';
-import Scanner from './src/Screens/QrScanner'
+import App from './App'
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => Scanner); 
+AppRegistry.registerComponent(appName, () => App); 
