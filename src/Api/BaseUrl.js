fd80f08726f327dc896d227831fab54b3a1546b2
anaaf <anaaf5555@gmail.com>
2022-06-20 00:50:14 +0500

    base url

---
+++ src/Api/BaseUrl.js
@@ -0,0 +1,5 @@
+//  export const baseUrl = 'http://192.168.0.102:3000';
+//  export const baseUrl = 'http://192.168.1.103:3000'
+ export const baseUrl = 'http://192.168.1.103:3000'
+
+//export const baseUrl = 'http://walletic-ap.herokuapp.com/api';
